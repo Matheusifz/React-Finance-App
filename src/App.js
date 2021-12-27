6bdda9bde1f46df127b095b027e4b9b0ddd25513
matheusifz <matheusifz@gmail.com>
2021-12-27 18:43:44 -0300

    feat: Create pages and add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import GlobalStyle from "./globalStyles";
+
+// Pages
+import Home from "./pages/Home";
+import SignUp from "./pages/SignupPage";
+import Pricing from "./pages/PricingPage";
+
 const App = () => {
   return (
-    <div className="Container">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <GlobalStyle />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
   );
 };
 
