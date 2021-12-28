c93676e321e743b83e59a597c01482b4ff6bcc62
matheusifz <matheusifz@gmail.com>
2021-12-28 14:44:02 -0300

    feat: create and styled Navbar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import GlobalStyle from "./globalStyles";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignupPage";
 import Pricing from "./pages/PricingPage";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <Router>
       <GlobalStyle />
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { FaTimes } from "react-icons/fa";
+import { CgMenuRight } from "react-icons/cg";
+import { IconContext } from "react-icons";
+
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavLinks,
+  NavItem,
+} from "./NavbarStyles";
+
+import { data } from "../../data/NavbarData";
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  let Navigate = useNavigate();
+  let location = useLocation();
+
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
+
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const closeMobileMenu = (to, id) => {
+    if (id && location.pathname === "/") {
+      scrollTo(id);
+    }
+
+    Navigate.push(to);
+    setShow(false);
+  };
+
+  return (
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            <NavIcon src="./assets/logo.png" alt="logo" />
+            Delta
+          </NavLogo>
+          <MobileIcon onClick={handleClick}>
+            {show ? <FaTimes /> : <CgMenuRight />}
+          </MobileIcon>
+          <NavMenu show={show}>
+            {data.map((el, index) => (
+              <NavItem key={index}>
+                <NavLinks onClick={() => closeMobileMenu(el.to, el.id)}>
+                  {el.text}
+                </NavLinks>
+              </NavItem>
+            ))}
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </IconContext.Provider>
+  );
+};
+
+export default Navbar;
