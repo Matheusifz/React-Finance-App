ab9135af91c811e1ea0259d36c86807021af025e
matheusifz <matheusifz@gmail.com>
2021-12-28 15:08:50 -0300

    feat: create and styled Feature component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Features from "../components/Features/Features";
 import Hero from "../components/Hero/Hero";
 
-// Hero Features Content Carousel
-
 const Home = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  );
 };
 
 export default Home;
